fdc1509fea2d26b50d03e7904450b9513fb80967
JanoBourian <supermath33@hotmail.com>
2023-07-05 12:15:48 -0600

    Add: doctest

--- 01-data-model/example1-1.py
+++ 01-data-model/example1-1.py
@@ -1,19 +1,49 @@
+"""
+This is an example of doctest
+>>> deck = FrenchDeck()
+>>> len(deck)
+52
+"""
+
 import collections
 from random import choice
 
 Card = collections.namedtuple('Card', ["rank", "suit"])
 
 class FrenchDeck:
+    """ Return a French deck 
+    
+    >>> deck = FrenchDeck()
+    >>> deck.__len__()
+    52
+
+    Returns:
+        FrenchDeck: A French deck
+    """
     ranks = [str(n) for n in range(2,11)] + list("JQKA")
     suites = "spades diamonds clubs hearts".split()
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
     
     def __init__(self):
+        """
+        >>> deck = FrenchDeck()
+        """
         self._cards = [Card(rank, suit) for suit in self.suites for rank in self.ranks]
         
     def __len__(self):
+        """
+        >>> deck = FrenchDeck()
+        >>> deck.__len__()
+        52
+        """
         return len(self._cards)
     
     def __getitem__(self, position):
+        """
+        >>> deck = FrenchDeck()
+        >>> deck.__getitem__(0)
+        Card(rank='2', suit='spades')
+        """
         return self._cards[position]
     
     def get_random_card(self):
@@ -23,3 +53,20 @@ deck = FrenchDeck()
 print(len(deck))
 print(choice(deck))
 print(deck.get_random_card())
+print(deck[:3])
+print(deck[12::13])
+
+for card in deck:
+    print(card.rank, card.suit, card)
+
+for card in reversed(deck):
+    print(card)
+
+print(deck._cards)
+
+if __name__ == "__main__":
+    # python file.py -v
+    import doctest
+    doctest.testmod()
+
+# python -m doctest -v file.py
